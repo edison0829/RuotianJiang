380c3a2ca0e67beb77b557a46c355cb56f80c4b0
Ruotian Jiang <ruotianj@usc.edu>
2019-05-20 14:10:01 -0700

    up

--- static/js/script.js
+++ static/js/script.js
@@ -72,20 +72,24 @@ function bringBackImage(){
 												if (query.length > 0)
 												{
 													var jsonld = JSON.parse(document.querySelector('script[type="application/ld+json"]').innerText);
-													console.log(jsonld.itemListElement);
 													var data =  jsonld.itemListElement;
 													var list = []
 													for(var i in data){
 														var values = $.map(data[i]['item'],function(value,key){return value});
-														var simi = []
+														var simi = [];
 														for (var j in values){
-															cur_sim = Math.round(similarity(query,values[j].toString())*10000)/100;
-															simi.push(cur_sim)
+															if (values[j].includes(query)){
+																cur_sim = 100;
 															}
-														var s_value = simi.sort((a,b) => a>b).slice(0,3);
-														var s_sum = s_value.reduce(function(a,b){return a+b;},0);
-														list.push({key:data[i],value:s_sum});
+															else
+															{
+															cur_sim = Math.round(similarity(query,values[j].toString())*10000)/100;
 														}
+															simi.push(cur_sim);
+														}
+														var s_sum = simi.reduce(function(a,b){return a+b;},0);
+														list.push({key:data[i],value:s_sum});
+													}
 													list.sort((a, b) => (a.value < b.value) ? 1 : -1)
 													console.log(list);
 													for(var i in list)
